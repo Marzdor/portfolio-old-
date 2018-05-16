b7b9273b495a69a363823397da78bea2fb73a3e4
Max Ingersoll <marzdor@gmail.com>
2018-05-15 17:15:17 -0700

    changed some mobile css to get ready for tablet

--- docs/assets/scripts/App.js
+++ docs/assets/scripts/App.js
@@ -3,11 +3,13 @@ window.onload = function () {
 }
 
 function genPortfolio() {
-  const baseNames = ["calculator", "pomodoroClock", "quote", "tictactoe", "travel", "twitchViewer", "weather", "wikipediaViewer"];
+  const baseNames = ["calculator", "localWeatherApp", "pomodoroClock", "quoteApp", "tictactoe", "travel-site-tut", "twitchStreamList", "wikipediaViewer"];
 
   for (let i = 0; i < baseNames.length; i++) {
     const markup = `
-    <img class="portfolio-image" src="assets/images/${baseNames[i]}_320x240.png" alt="Thumbnail of ${baseNames[i]} website.">
+    <a href="https://marzdor.github.io/${baseNames[i]}">
+      <img class="portfolio-image" src="assets/images/${baseNames[i]}_320x240.png" alt="Thumbnail of ${baseNames[i]} website.">
+    </a>
     `;
     const target = document.querySelector(".portfolio");
     target.insertAdjacentHTML("beforeend", markup);
